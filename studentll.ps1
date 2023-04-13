0a7cef0bf143cb628bda0427085b36024e7b5b7d
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2023-04-13 06:53:04 -0700

    Update studentll.ps1

--- studentll.ps1
+++ studentll.ps1
@@ -1,3 +1,3 @@
 New-LocalUser -Name StudentLL -NoPassword -UserMayNotChangePassword
 Set-LocalUser -Name StudentLL -PasswordNeverExpires $true
-dd-LocalGroupMember -Group "Users" -Member "StudentLL"
+Add-LocalGroupMember -Group "Users" -Member "StudentLL"
