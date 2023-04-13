609d98a409e9aaa24f614788772db244ea73e036
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2023-04-13 07:04:08 -0700

    Create returnwar.ps1

---
+++ returnwar.ps1
@@ -0,0 +1,2 @@
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "legalnoticecaption" -Value "Please Return Your CAC Laptop"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "legalnoticetext" -Value "*** Your account will be disable in 7 days if not returned. Please contact your local CAC Library for instructions.***"
