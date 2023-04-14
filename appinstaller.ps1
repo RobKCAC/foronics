68e7cfefcd5bb743fabf0d5396fa02ced7443499
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2023-04-14 10:02:44 -0700

    Update appinstaller.ps1

--- appinstaller.ps1
+++ appinstaller.ps1
@@ -1,1 +1,5 @@
+Set-Service -Name UsoSvc -StartupType Automatic
+Set-Service -Name wuauserv -StartupType Automatic
+Set-Service -Name UsoSvc -Status Running
+Set-Service -Name wuauservc -Status Running
 Get-AppxPackage "AppInstaller" | add-appxpackage
