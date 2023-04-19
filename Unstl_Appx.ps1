e46c2dfbe5369e4077c93c12d3f675d275ef3007
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2023-04-19 10:13:05 -0700

    Update Unstl_Appx.ps1

--- Unstl_Appx.ps1
+++ Unstl_Appx.ps1
@@ -53,3 +53,11 @@
 Get-AppxProvisionedPackage -Online | Select PackageName | sort
 Start-Sleep -Seconds 120
 
+Set-Service -Name UsoSvc -StartupType Automatic
+Set-Service -Name wuauserv -StartupType Automatic
+Set-Service -Name UsoSvc -Status Running
+Set-Service -Name wuauserv -Status Running
+Get-AppxPackage "AppInstaller" | add-appxpackage
+
+Start-Sleep -Seconds 240
+
