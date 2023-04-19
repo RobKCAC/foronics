0887f4ae5b4bfdb85a8f088d30aaadf214112fd4
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2023-04-19 10:22:23 -0700

    Update officeinst.ps1

--- officeinst.ps1
+++ officeinst.ps1
@@ -2,4 +2,5 @@ Set-Service -Name UsoSvc -StartupType Automatic
 Set-Service -Name wuauserv -StartupType Automatic
 Set-Service -Name UsoSvc -Status Running
 Set-Service -Name wuauserv -Status Running
+Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
 winget install -q Microsoft.Office  --accept-source-agreements
