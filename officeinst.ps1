ea75cdae997fbb9f55aa4ae74304b5b77bbe1776
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2023-04-25 07:58:11 -0700

    Update officeinst.ps1

--- officeinst.ps1
+++ officeinst.ps1
@@ -3,4 +3,3 @@ Set-Service -Name wuauserv -StartupType Automatic
 Set-Service -Name UsoSvc -Status Running
 Set-Service -Name wuauserv -Status Running
 winget install Microsoft.Office --silent --accept-package-agreements --accept-source-agreements
-https://github.com/RobKCAC/foronics/blob/main/officeinst.ps1
