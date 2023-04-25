2a57d3b9eef25776f1041eb49d56284701b32f55
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2023-04-25 08:13:39 -0700

    Update officeinst.ps1

--- officeinst.ps1
+++ officeinst.ps1
@@ -2,4 +2,4 @@ Set-Service -Name UsoSvc -StartupType Automatic
 Set-Service -Name wuauserv -StartupType Automatic
 Set-Service -Name UsoSvc -Status Running
 Set-Service -Name wuauserv -Status Running
-winget install Microsoft.Office --silent --accept-package-agreements
+winget install Microsoft.Office --silent --accept-package-agreements --accept-source-agreements
