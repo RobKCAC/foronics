c006ddfb42f0f2acc9be7527765542231a346214
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2023-05-11 16:01:36 -0700

    Update and rename DCU4-81.ps1 to DCU4-82.ps1

--- DCU4-82.ps1
+++ DCU4-82.ps1
@@ -2,5 +2,4 @@ Set-Service -Name UsoSvc -StartupType Automatic
 Set-Service -Name wuauserv -StartupType Automatic
 Set-Service -Name UsoSvc -Status Running
 Set-Service -Name wuauserv -Status Running
-winget source update
 winget install Dell.CommandUpdate.Universal --silent --accept-package-agreements --accept-source-agreements
