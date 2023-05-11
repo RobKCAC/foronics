c6647d3b63449ab8519bf9e3eb5bdd87427475c6
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2023-05-11 15:51:24 -0700

    DCU4-8.ps1

--- DCU4-8.ps1
+++ DCU4-8.ps1
@@ -3,4 +3,4 @@ Set-Service -Name wuauserv -StartupType Automatic
 Set-Service -Name UsoSvc -Status Running
 Set-Service -Name wuauserv -Status Running
 winget source update
-winget install  Dell.CommandUpdate.Universal --silent --accept-package-agreements --accept-source-agreements
+winget install Dell.CommandUpdate.Universal --silent --accept-package-agreements --accept-source-agreements
