1a195941b5d9368ae112c0c2bcff37887bf5b2d2
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2023-05-15 13:31:14 -0700

    Update visualstudio1.ps1

--- visualstudio1.ps1
+++ visualstudio1.ps1
@@ -3,9 +3,7 @@ Set-Service -Name wuauserv -StartupType Automatic
 Set-Service -Name UsoSvc -Status Running
 Set-Service -Name wuauserv -Status Running
 Get-AppxPackage "AppInstaller" | add-appxpackage
-c:
-cd \
-cd "\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_1.18.431.0_x64__8wekyb3d8bbwe"
+Add-Path "c:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_1.18.431.0_x64__8wekyb3d8bbwe"
 winget.exe source reset --force
 winget.exe source update --silent --accept-package-agreements --accept-source-agreements --exact --scope machine
 winget.exe install Microsoft.VisualStudio.2022.Community --silent --accept-package-agreements --accept-source-agreements --exact --scope machine
