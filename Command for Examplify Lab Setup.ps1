8c6bca3fabd30801ad398358547bba65f487f7ad
CirkulWaterBottle <151052505+CirkulWaterBottle@users.noreply.github.com>
2024-01-18 11:34:01 -0700

    Update Command for Examplify Lab Setup.ps1

--- Command for Examplify Lab Setup.ps1
+++ Command for Examplify Lab Setup.ps1
@@ -1,1 +1,3 @@
-.\Examplify_3.4.2_win.exe /s /v" /qn REBOOT=ReallySuppress" /z"C:\ExamSoft\setup.json"
+md "c:\Examplify"
+xcopy /H /E /Y "\\spc-mdt01\1809DeploymentShare\Applications\_FDeploy+FDeepFreeze.Apps_\Examplify" "C:\Examplify"
+.\Examplify_3.4.2_win.exe /s /v" /qn REBOOT=ReallySuppress" /z"C:\Examplify\simple-setup.json"
