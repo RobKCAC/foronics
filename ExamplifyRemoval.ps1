2a17482a31021a20b132c90ee86dfd7c5ac64ec3
CirkulWaterBottle <151052505+CirkulWaterBottle@users.noreply.github.com>
2024-01-18 16:52:08 -0700

    Update ExamplifyRemoval.ps1

--- ExamplifyRemoval.ps1
+++ ExamplifyRemoval.ps1
@@ -1,1 +1,2 @@
 Remove-Item -Path "C:\Program Files (x86)\InstallShield Installation Information\{9C28E5C5-53AB-4488-8601-839F3116C941}" -Force -Recurse
+Remove-Item -Path "C:\ProgramData\Examplify" -Force -Recurse
