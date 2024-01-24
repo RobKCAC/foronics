5d313bc7b123dfed0e0f932909f59a080b6476f3
CirkulWaterBottle <151052505+CirkulWaterBottle@users.noreply.github.com>
2024-01-24 09:02:52 -0700

    Update ExamplifyRemoval.ps1

--- ExamplifyRemoval.ps1
+++ ExamplifyRemoval.ps1
@@ -1,4 +1,4 @@
 Remove-Item -Path "C:\Program Files (x86)\InstallShield Installation Information\{9C28E5C5-53AB-4488-8601-839F3116C941}" -Force -Recurse
 Remove-Item -Path "C:\ProgramData\Examplify" -Force -Recurse
-Remove-Item -Path "C:\Users\Public\Documents\.adata -Force -Recurse
-Remove-Item -Path "C:\Users\Public\Documents\.adata-Examplify -Force -Recurse
+Remove-Item -Path "C:\Users\Public\Documents\.adata" -Force -Recurse
+Remove-Item -Path "C:\Users\Public\Documents\.adata-Examplify" -Force -Recurse
