0fa7c2220b524d11f3dd145a2fc9486f4cac7787
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2024-03-28 07:14:53 -0700

    Update CACgamers.ps1

--- CACgamers.ps1
+++ CACgamers.ps1
@@ -1,3 +1,4 @@
 New-LocalUser -Name CAC_Gamers -NoPassword -UserMayNotChangePassword
 Set-LocalUser -Name CAC_Gamers -PasswordNeverExpires $true
 Add-LocalGroupMember -Group "Users" -Member "CAC_Gamers"
+Add-LocalGroupMember -Group "administrators" -Member "CAC_Gamers"
