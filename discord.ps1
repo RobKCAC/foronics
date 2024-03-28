e9aabd11584c0794bfd6350ee8364577ef0a6e3d
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2024-03-28 08:03:39 -0700

    Update discord.ps1

--- discord.ps1
+++ discord.ps1
@@ -1,1 +1,3 @@
+winget.exe source reset --force
+winget.exe source update --silent --accept-package-agreements --accept-source-agreements --exact --scope machine
 winget.exe install discord.discord --silent --accept-package-agreements --accept-source-agreements
